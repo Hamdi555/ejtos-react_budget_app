64f8127a99b33447f6ee974f89553ecf03728d41
Hamdi555 <115834403+Hamdi555@users.noreply.github.com>
2022-12-25 01:16:47 +0100

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,54 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
+    const { expenses } = useContext(AppContext);
+    const { currency } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const increaseBudget = () => {
+        let newBudget = budget + 10;
+        changeBudget(newBudget);
+    }
+
+    const decreaseBudget = () => {
+        let newBudget = budget - 10;
+        changeBudget(newBudget);
+    }
+
+    const changeBudget = (newBudget) => {
+        if (newBudget > 20000){
+            alert("The value cannot exceed 20.000!");
+        }else if (newBudget < totalExpenses){
+            alert("You can't reduce the budget lower than the spending");
+        }else{
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: newBudget
+            });
+        }
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency} </span>
+            <input
+                required='required'
+                type='number'
+                id='cost'
+                value={budget}
+                style={{ size: 2}}
+                onChange={(event) => changeBudget(parseInt(event.target.value))}>
+            </input>
+            <button onClick={event=> decreaseBudget()}>-</button>
+            <button onClick={event=> increaseBudget()}>+</button>
+        </div>
+    );
+};
+
+export default Budget;
